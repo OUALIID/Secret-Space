b2783fbaa48239e9fe52f86d63fb4b38724ea89b
OUALIID <walidelhadim8@gmail.com>
2024-10-24 16:01:36 +0100

    Add function to update label based on device connection status.

--- secret_space.py
+++ secret_space.py
@@ -6,3 +6,12 @@ import time
 
 # Global variables
 check_button_clicked = False
+
+# Function to update label image
+def update_label_image(image_path):
+    global image
+    image = tk.PhotoImage(file=image_path)
+    label.config(image=image)
+    label.image = image
+    label.lift()
+
