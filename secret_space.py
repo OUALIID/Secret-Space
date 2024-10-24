5435cb154154fe6484253e49286407ae8a8e4163
OUALIID <walidelhadim8@gmail.com>
2024-10-24 16:05:59 +0100

    Add function to display UI elements when device is connected.

--- secret_space.py
+++ secret_space.py
@@ -24,3 +24,27 @@ def update_label():
         show_disconnected()
     root.after(1000, update_label)
 
+# Function to display UI elements when device is connected
+def show_connected():
+    update_label_image("/home/oualid/Documents/piplo/zimage/connected_device.png")
+    root.geometry("360x390")
+    root.configure(bg="#141313")
+    label.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+    canvas.config(bg="black")
+    canvas.pack(side=tk.BOTTOM)
+
+    # Display device information
+    device_info = get_device_info()
+    for i, info in enumerate(device_info):
+        result_label = tk.Label(root, text=info, font=("Helvetica", 10, "bold"), bg="#787878", fg="white")
+        result_label.place(relx=0.68, rely=0.10 + i * 0.08, anchor="center")
+    
+    # Show the Group3.png image
+    image_label.place(relx=0.5, rely=0.18, anchor="center")
+
+    # Show the check button
+    check_button.place(relx=0.3, rely=0.35)
+
+    # Show the Start button
+    start_button_label.place(relx=0.35, rely=0.43)
+
